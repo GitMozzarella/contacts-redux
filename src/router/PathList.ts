de184fd529d5607eb8c00e815bb29ca42f6eac87
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-06-30 12:48:49 +0500

    chore: add enum for path location of router

---
+++ src/router/PathList.ts
@@ -0,0 +1,8 @@
+export enum PathList {
+	home = '/',
+	allContacts = '/contacts',
+	contact = '/contacts/:contactId',
+	allGroups = '/groups',
+	group = '/groups/:groupId',
+	favorite = '/favorite'
+}
