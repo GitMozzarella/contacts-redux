ff92a9a7ca2b6211d6a0f40fb3fd496f51a38280
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-07-24 21:24:07 +0500

    feat: убрал контекст и вставит provider store

--- src/apps/MainApp/MainApp.tsx
+++ src/apps/MainApp/MainApp.tsx
@@ -1,14 +1,14 @@
 import { Provider } from 'react-redux'
-import { ContactsProvider } from 'src/context/ContactsContext'
+// import { ContactsProvider } from 'src/context/ContactsContext'
 import { store } from 'src/redux/store'
 import { Router } from 'src/router'
 
 export const MainApp = () => {
 	return (
 		<Provider store={store}>
-			<ContactsProvider>
-				<Router />
-			</ContactsProvider>
+			{/* <ContactsProvider> */}
+			<Router />
+			{/* </ContactsProvider> */}
 		</Provider>
 	)
 }
