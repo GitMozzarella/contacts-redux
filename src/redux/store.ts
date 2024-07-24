26f90dc560ce7f334fe2a023e2bca732ab7772a5
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-07-24 22:18:27 +0500

    feat: добавил новый редьюсер для управления состояния фильтрации

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,12 +3,14 @@ import { contactsReducer } from './reducers/contactsReducer'
 import { favoriteContactsReducer } from './reducers/favoriteContactsReducer'
 import { groupContactsReducer } from './reducers/groupContactsReducer'
 import { filteredFavoriteContactsReducer } from './reducers/filteredFavoriteContactsReducer'
+import { filterReducer } from './reducers/filterReducer'
 
 export const rootReducer = combineReducers({
 	contacts: contactsReducer,
 	favoriteContacts: favoriteContactsReducer,
 	groupContacts: groupContactsReducer,
-	filteredFavoriteContacts: filteredFavoriteContactsReducer
+	filteredFavoriteContacts: filteredFavoriteContactsReducer,
+	filter: filterReducer
 })
 
 export const store = createStore(rootReducer)
