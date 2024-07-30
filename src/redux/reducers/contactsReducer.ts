198a27b457e9be1b1c64f897271b0f03d1f88511
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-07-30 14:25:03 +0500

    feat: case add_contact

--- src/redux/reducers/contactsReducer.ts
+++ src/redux/reducers/contactsReducer.ts
@@ -1,4 +1,8 @@
-import { DELETE_CONTACT, SET_CONTACTS } from '../actions/actionTypes'
+import {
+	DELETE_CONTACT,
+	SET_CONTACTS,
+	ADD_CONTACT
+} from '../actions/actionTypes'
 import { ContactDto } from 'src/types/dto/ContactDto'
 import { DATA_CONTACT } from 'src/data'
 import { ProjectActions } from '../actions/actions'
@@ -16,6 +20,9 @@ export const contactsReducer = (
 		case DELETE_CONTACT:
 			const contactIdToDelete = action.payload
 			return state.filter(contact => contact.id !== contactIdToDelete)
+
+		case ADD_CONTACT:
+			return [...state, action.payload]
 
 		default:
 			return state
