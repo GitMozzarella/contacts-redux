60d5ed2e6adc1fb40d88fac77268e68fac45a50d
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-08-17 19:38:33 +0500

    feat: logger метрик

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,19 +1,23 @@
-import { combineReducers, createStore } from 'redux'
+import { applyMiddleware, combineReducers, createStore } from 'redux'
 import { contactsReducer } from './reducers/contactsReducer'
 import { favoriteContactsReducer } from './reducers/favoriteContactsReducer'
 import { groupContactsReducer } from './reducers/groupContactsReducer'
 import { filterReducer } from './reducers/filterReducer'
 import { authReducer } from './reducers/authReducer'
+import { logActionMiddleware } from './logActionMiddleware'
+
+const rootReducer = combineReducers({
+	contacts: contactsReducer,
+	favoriteContacts: favoriteContactsReducer,
+	groupContacts: groupContactsReducer,
+	filter: filterReducer,
+	auth: authReducer
+})
 
 export const store = createStore(
-	combineReducers({
-		contacts: contactsReducer,
-		favoriteContacts: favoriteContactsReducer,
-		groupContacts: groupContactsReducer,
-		filter: filterReducer,
-		auth: authReducer
-	})
+	rootReducer,
+	applyMiddleware(logActionMiddleware) as any
 )
 
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof rootReducer>
 export type AppDispatch = typeof store.dispatch
