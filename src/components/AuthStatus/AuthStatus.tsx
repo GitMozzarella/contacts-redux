ee356daf601e0caf2fb66683f6a299e230210987
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-08-19 02:01:42 +0500

    chore: временно убрал приватный роутинг и статус авторизации

--- src/components/AuthStatus/AuthStatus.tsx
+++ src/components/AuthStatus/AuthStatus.tsx
@@ -1,35 +1,33 @@
-import { useNavigate } from 'react-router-dom'
-import styles from './status.module.scss'
-import { MdLogout } from 'react-icons/md'
-import { FaUserAlt } from 'react-icons/fa'
-import { PathList } from '../../router/PathList'
+// import { useNavigate } from 'react-router-dom'
+// import styles from './status.module.scss'
+// import { MdLogout } from 'react-icons/md'
+// import { FaUserAlt } from 'react-icons/fa'
+// import { PathList } from '../../router/PathList'
+// import { useAppDispatch, useAppSelector } from 'src/redux/hooks'
 
-import { setAuthActionCreator } from '../../redux/actions/actions'
-import { useAppDispatch, useAppSelector } from 'src/redux/hooks'
+// export function AuthStatus() {
+// 	const dispatch = useAppDispatch()
+// 	const navigate = useNavigate()
 
-export function AuthStatus() {
-	const dispatch = useAppDispatch()
-	const navigate = useNavigate()
+// 	const user = useAppSelector(state => state.auth.user)
 
-	const user = useAppSelector(state => state.auth.user)
+// 	const handleSignOut = () => {
+// 		dispatch(setAuth({ user: null }))
+// 		navigate(PathList.auth)
+// 	}
 
-	const handleSignOut = () => {
-		dispatch(setAuthActionCreator({ user: null }))
-		navigate(PathList.auth)
-	}
+// 	return (
+// 		<>
+// 			<div className={styles.message}>
+// 				<span className={styles.logoutButton}>
+// 					<FaUserAlt />
+// 				</span>
 
-	return (
-		<>
-			<div className={styles.message}>
-				<span className={styles.logoutButton}>
-					<FaUserAlt />
-				</span>
-
-				<p className={styles.username}>'{user}'</p>
-				<button onClick={handleSignOut} className={styles.logoutButton}>
-					<MdLogout />
-				</button>
-			</div>
-		</>
-	)
-}
+// 				<p className={styles.username}>'{user}'</p>
+// 				<button onClick={handleSignOut} className={styles.logoutButton}>
+// 					<MdLogout />
+// 				</button>
+// 			</div>
+// 		</>
+// 	)
+// }
