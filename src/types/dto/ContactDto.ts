3e69bacf97d1120ca47b5ec4ca84f84ba0292792
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-08-22 18:25:29 +0500

    fix: добавил docId в тип ContactDto

--- src/types/dto/ContactDto.ts
+++ src/types/dto/ContactDto.ts
@@ -1,13 +1,14 @@
 export interface ContactDto {
-  id: string;
-  /** телефон */
-  phone: string
-  /** имя */
-  name: string
-  /** дата рождения */
-  birthday: string
-  /** адрес */
-  address: string
-  /** фото */
-  photo: string
+	docId: string
+	id: string
+	/** телефон */
+	phone: string
+	/** имя */
+	name: string
+	/** дата рождения */
+	birthday: string
+	/** адрес */
+	address: string
+	/** фото */
+	photo: string
 }
