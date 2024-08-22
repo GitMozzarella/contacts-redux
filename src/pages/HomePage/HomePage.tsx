de0ab296b0c9236e468aad8976d7d944b3d25dee
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-08-22 15:43:32 +0500

    refactor: вынес строки в отдельный файл

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,11 @@
 import { useAppStore } from 'src/redux/hooks'
 import styles from './home.module.scss'
+import {
+	HOME_PAGE_TITLE,
+	HOME_PAGE_DESCRIPTION,
+	HOME_PAGE_FEATURES,
+	HOME_PAGE_INSTRUCTIONS
+} from '../../constants/homePageMarkup'
 
 export const HomePage = () => {
 	const store = useAppStore()
@@ -7,26 +13,17 @@ export const HomePage = () => {
 	const onHomePageClick = () => {
 		console.log('Вы кликнули по домашней странице', store.getState())
 	}
+
 	return (
 		<div className={styles.container} onClick={onHomePageClick}>
-			<h1>Добро пожаловать!</h1>
-			<p>
-				Это домашняя страница вашего приложения контакты. Здесь вы можете
-				управлять всеми вашими контактами и группами контактов. Приложение
-				позволяет вам:
-			</p>
+			<h1>{HOME_PAGE_TITLE}</h1>
+			<p>{HOME_PAGE_DESCRIPTION}</p>
 			<ul>
-				<li>Добавлять, редактировать и удалять контакты</li>
-				<li>Создавать и управлять группами контактов</li>
-				<li>Отмечать избранные контакты для быстрого доступа</li>
-				<li>Поиск контактов по имени или другим параметрам</li>
+				{HOME_PAGE_FEATURES.map((feature, index) => (
+					<li key={index}>{feature}</li>
+				))}
 			</ul>
-			<p>
-				Для начала работы, используйте меню навигации вверху страницы. Вы можете
-				перейти к списку всех контактов, управлять группами или просмотреть
-				избранные контакты. Мы стремимся сделать управление вашими контактами
-				как можно более удобным и интуитивно понятным.
-			</p>
+			<p>{HOME_PAGE_INSTRUCTIONS}</p>
 		</div>
 	)
 }
