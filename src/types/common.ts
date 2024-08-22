7c701b615884e232b2561b82b48d0ebd8409bc31
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-08-22 23:14:11 +0500

    feat: types/interface for ErrorBoundary RouteWithErrorBoundary

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,5 +1,15 @@
-import {Dispatch, SetStateAction} from 'react';
+import { Dispatch, ReactNode, SetStateAction } from 'react'
 
-export type State<TState> = [
-  TState, Dispatch<SetStateAction<TState>>
-]
+export type State<TState> = [TState, Dispatch<SetStateAction<TState>>]
+
+export interface ErrorBoundaryProps {
+	children: ReactNode
+}
+
+export interface ErrorBoundaryState {
+	hasError: boolean
+}
+
+export interface RouteWithErrorBoundaryProps {
+	children: React.ReactNode
+}
