9547ec43462299599ed6418f288ba5fda5caf67e
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-08-26 11:38:04 +0500

    fix: получаю данные теперь в rtk-query

--- src/redux/slices/contactsSlice.ts
+++ src/redux/slices/contactsSlice.ts
@@ -1,165 +1,77 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { ContactDto } from 'src/types/dto/ContactDto'
-import { GroupContactsDto } from 'src/types/dto/GroupContactsDto'
+
 import { FilterFormValues } from 'src/components/FilterForm/FilterForm'
-import {
-	fetchContactsFromFirestore,
-	fetchGroupContactsFromFirestore,
-	addContactFirestore,
-	editContactFirestore,
-	deleteContactFirestore
-} from '../asyncActions/asyncActions'
+import { contactsApiSlice } from '../rtkQuery/contacts/slice'
 
 interface ContactsState {
 	contacts: ContactDto[]
-	favoriteContacts: string[]
-	groupContacts: GroupContactsDto[]
+
 	filter: Partial<FilterFormValues>
 	loading: boolean
 	error: string | null
+	favoriteContacts: string[]
 }
 
 const initialState: ContactsState = {
 	contacts: [],
-	favoriteContacts: [],
-	groupContacts: [],
+
 	filter: {},
 	loading: false,
-	error: null
+	error: null,
+	favoriteContacts: []
 }
 
 const contactsSlice = createSlice({
 	name: 'contacts',
 	initialState,
 	reducers: {
-		setContacts(state, action: PayloadAction<ContactDto[]>) {
-			state.contacts = action.payload
-		},
-		setFavoriteContacts(state, action: PayloadAction<string[]>) {
-			state.favoriteContacts = action.payload
-		},
-		setGroupContacts(state, action: PayloadAction<GroupContactsDto[]>) {
-			state.groupContacts = action.payload
-		},
 		setFilterValues(state, action: PayloadAction<Partial<FilterFormValues>>) {
 			state.filter = action.payload
 		},
+
 		toggleFavoriteContact(state, action: PayloadAction<string>) {
 			const contactId = action.payload
-			if (state.favoriteContacts.includes(contactId)) {
-				state.favoriteContacts = state.favoriteContacts.filter(
-					id => id !== contactId
-				)
+			const favoriteSet = new Set(state.favoriteContacts)
+			if (favoriteSet.has(contactId)) {
+				favoriteSet.delete(contactId)
 			} else {
-				state.favoriteContacts.push(contactId)
+				favoriteSet.add(contactId)
 			}
-		},
-		deleteContactStore(state, action: PayloadAction<string>) {
-			state.contacts = state.contacts.filter(
-				contact => contact.id !== action.payload
-			)
-		},
-		addContactStore(state, action: PayloadAction<ContactDto>) {
-			state.contacts.push(action.payload)
-		},
-		editContactStore(state, action: PayloadAction<ContactDto>) {
-			const index = state.contacts.findIndex(
-				contact => contact.id === action.payload.id
-			)
-			if (index !== -1) {
-				state.contacts[index] = action.payload
-			}
+			state.favoriteContacts = Array.from(favoriteSet)
 		}
 	},
 	extraReducers: builder => {
 		builder
-			.addCase(fetchContactsFromFirestore.pending, state => {
-				state.loading = true
-				state.error = null
-			})
-			.addCase(fetchContactsFromFirestore.fulfilled, (state, action) => {
-				state.loading = false
-				state.contacts = action.payload
-			})
-			.addCase(fetchContactsFromFirestore.rejected, (state, action) => {
-				state.loading = false
-				state.error = action.payload as string
-			})
-			.addCase(fetchGroupContactsFromFirestore.pending, state => {
-				state.loading = true
-				state.error = null
-			})
-			.addCase(fetchGroupContactsFromFirestore.fulfilled, (state, action) => {
-				state.loading = false
-				state.groupContacts = action.payload
-			})
-			.addCase(fetchGroupContactsFromFirestore.rejected, (state, action) => {
-				state.loading = false
-				state.error = action.payload as string
-			})
-			.addCase(addContactFirestore.pending, state => {
-				state.loading = true
-				state.error = null
-			})
-			.addCase(addContactFirestore.fulfilled, (state, action) => {
-				state.loading = false
-
-				const newContact: ContactDto = {
-					...action.meta.arg,
-					id: action.payload.id
+			.addMatcher(
+				contactsApiSlice.endpoints.getContacts.matchPending,
+				state => {
+					state.loading = true
+					state.error = null
 				}
-
-				state.contacts.push(newContact)
-			})
-			.addCase(addContactFirestore.rejected, (state, action) => {
-				state.loading = false
-				state.error = action.payload as string
-			})
-			.addCase(editContactFirestore.pending, state => {
-				state.loading = true
-				state.error = null
-			})
-			.addCase(editContactFirestore.fulfilled, (state, action) => {
-				state.loading = false
-				const updatedContact = action.meta.arg
-				const index = state.contacts.findIndex(
-					contact => contact.id === updatedContact.id
-				)
-				if (index !== -1) {
-					state.contacts[index] = updatedContact
+			)
+			.addMatcher(
+				contactsApiSlice.endpoints.getContacts.matchFulfilled,
+				(state, action: PayloadAction<ContactDto[]>) => {
+					state.contacts = action.payload
+					state.loading = false
 				}
-			})
-			.addCase(editContactFirestore.rejected, (state, action) => {
-				state.loading = false
-				state.error = action.payload as string
-			})
-			.addCase(deleteContactFirestore.pending, state => {
-				state.loading = true
-				state.error = null
-			})
-			.addCase(deleteContactFirestore.fulfilled, (state, action) => {
-				state.loading = false
-				const contactId = action.meta.arg
-				state.contacts = state.contacts.filter(
-					contact => contact.id !== contactId
-				)
-			})
-			.addCase(deleteContactFirestore.rejected, (state, action) => {
-				state.loading = false
-				state.error = action.payload as string
-			})
+			)
+			.addMatcher(
+				contactsApiSlice.endpoints.getContacts.matchRejected,
+				(state, action) => {
+					if (action.payload) {
+						state.error =
+							(action.payload.data as string) || 'Unknown error occurred'
+					} else {
+						state.error = 'An error occurred'
+					}
+					state.loading = false
+				}
+			)
 	}
 })
 
-export const {
-	setContacts,
-	setFavoriteContacts,
-	setGroupContacts,
-	setFilterValues,
-	toggleFavoriteContact,
-	deleteContactStore,
-	addContactStore,
-	editContactStore
-} = contactsSlice.actions
+export const { setFilterValues, toggleFavoriteContact } = contactsSlice.actions
 
 export const contactsReducer = contactsSlice.reducer
