b6e590498b3fe1b23a72864984cd3323f36749cc
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-08-26 11:35:51 +0500

    chore:  заглушка для профиля без фото

---
+++ src/components/PlaceholderImage/PlaceholderImage.tsx
@@ -0,0 +1,10 @@
+export const PlaceholderImage = () => {
+	return (
+		<img
+			width='50'
+			height='50'
+			src='https://img.icons8.com/ios/50/gender-neutral-user--v1.png'
+			alt='gender-neutral-user--v1'
+		/>
+	)
+}
